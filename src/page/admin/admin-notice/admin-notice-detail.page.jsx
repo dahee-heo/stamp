ff8b6e29c4b29984dcc8b2d084ee418996a25a18
dahee-heo <hdahee.9@gmail.com>
2023-05-07 00:32:12 +0900

    admin 페이지 notice detail 페이지 댓글 작성 기능

--- src/page/admin/admin-notice/admin-notice-detail.page.jsx
+++ src/page/admin/admin-notice/admin-notice-detail.page.jsx
@@ -1,22 +1,36 @@
-import { Button, FormControl, Input, Textarea } from '@chakra-ui/react'
+import { 
+  Button, 
+  FormControl, 
+  Input, 
+  Textarea, 
+} from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react'
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import './admin-notice-regist.scss'
 import { useNavigate, useParams } from 'react-router-dom';
 import ReactHtmlParser from "react-html-parser";
-import { noticeGet } from '../../../service/notice.service';
+import { commentGet, commentRegist, noticeGet } from '../../../service/notice.service';
+import { useRecoilState } from 'recoil';
+import { authState } from '../../../atom/auth.atom';
 
 const AdminNoticeDetailPage = () => {
+  const [auth, setAuth] = useRecoilState(authState)
+  const navigate = useNavigate();
+  const params = useParams();
   const [contents, setContents] = useState({
     title: '',
     content: '',
   })
-  const navigate = useNavigate();
-  const params = useParams();
-
+  const [comment, setComment] = useState({
+    content: '',
+    date: new Date(),
+    noticeId: params.id 
+  })
+  const [commentList, setCommentList] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  
   useEffect(() => {
     getNotice()
+    getComment()
   }, [])
 
   const getNotice = async () => {
@@ -24,12 +38,33 @@ const AdminNoticeDetailPage = () => {
     setContents(data)
   }
 
-  const handleTitleChange = e => {
-    setContents({
-      ...contents,
-      title: e.target.value
+  const handleCommentChange = e => {
+    setComment({
+      ...comment,
+      content: e.target.value
     })
   }
+
+  const handleCommentSubmit = async () => {
+    await commentRegist(comment)
+    setComment({ content: '' })
+    getComment()
+  }
+
+  const getComment = async () => {
+    const { data } = await commentGet(params.id)
+    setCommentList( data )
+  }
+
+  const list = commentList?.filter( comment => {
+    return comment.noticeId === params.id
+  })
+
+  const handleCommentEdit = async (e) => {
+    
+  }
+
+  
 
   return (
     <>
@@ -40,9 +75,35 @@ const AdminNoticeDetailPage = () => {
       <div>
         댓글
         <FormControl>
-          <Textarea></Textarea>
-          <Button>작성</Button>
+          <Textarea 
+            onChange={handleCommentChange}
+            placeholder='댓글을 입력하세요.'
+            value={comment.content}
+          ></Textarea>
+          <Button onClick={handleCommentSubmit}>작성</Button>
         </FormControl>
+
+        
+        { list?.map(comment => {
+          return (
+            <div key={comment._id}>
+              {isEdit ? (
+                <div>
+                  <Input></Input>
+                  <Button>수정</Button>
+                  <Button>취소</Button>
+                </div>
+              ) : (
+                <div>
+                  {comment.userId.name}
+                  {comment.date}
+                  {comment.content}
+                  <Button>수정</Button>
+                </div>
+              )}
+            </div>
+          )
+        })}
       </div>
     </>
   )
--- src/service/notice.service.js
+++ src/service/notice.service.js
@@ -4,10 +4,12 @@ import axios from "axios";
 
 const hostUrl = getHostUrl()
 
+//notice 게시글
 export const noticeGetList = async (paginationMeta) => {
   const url = getSearchUrl(`${hostUrl}/notice`, paginationMeta)
   return await axios.get(url)
 }
+
 export const noticeGet = async (noticeGetParams) => {
   const url = getSearchUrl(`${hostUrl}/notice/${noticeGetParams}`)
   return await axios.get(url)
@@ -23,4 +25,13 @@ export const noticeUpdate = async (noticeUpdateParams) => {
 
 export const noticeDelete = async (noticeDeleteId) => {
   return await axios.delete(`${hostUrl}/notice/${noticeDeleteId}`)
+}
+
+//notice 댓글
+export const commentRegist = async (noticeCommentRegistParams) => {
+  return await axios.post(`${hostUrl}/notice/${noticeCommentRegistParams.noticeId}/comment`, noticeCommentRegistParams)
+}
+
+export const commentGet = async (noticeCommentGetParams) => {
+  return await axios.get(`${hostUrl}/notice/${noticeCommentGetParams}/comment`)
 }
